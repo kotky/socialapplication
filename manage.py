b59980f4d603f6f5060ee7857063de0a466fc7f7
kotky <kotaraca@net.hr>
2015-11-10 15:24:41 +0100

    dodano automatsko startanje crossbara kad se pokrene manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialapplication.settings")
-
     from django.core.management import execute_from_command_line
-
+    process = subprocess.Popen(['crossbar', 'start'])
     execute_from_command_line(sys.argv)
+
+
+
